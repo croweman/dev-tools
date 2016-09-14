19846881a9945da7507dbef26c7ab00c42ca74ba
lee crowe <lee.crowe@comparethemarket.com>
2016-09-14 11:05:04 +0100

    Change to pre commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,11 +2,25 @@
 
 set -e
 
+####################################################
+# SETUP START
+#
+# global git hooks setup
+#  - git config --global init.templatedir '~/.git-templates'
+#  - mkdir -p ~/.git-templates/hooks
+#  - create ~/.git-templates/hooks/pre-commit file and populate it with this bash script
+#  - make file executable
+#      chmod a+x ~/.git-templates/hooks/pre-commit
+#  - reinitialise each relevant git hub repo that should use the script
+#      git init
+
 # jq is a dependency which will need to be installed
 #   https://stedolan.github.io/jq/  osx
 #   - copy file to /usr/local/bin
 #   - make file executable
 #     chmod +x jq
+# SETUP END
+####################################################
 
 if [ ! -f ./deploy/metadata.json ]; then
     exit
@@ -17,3 +31,4 @@ lastVersionPart="${currentVersion##*.}"
 lastVersionPart=$((lastVersionPart + 1))
 newVersion=${currentVersion%.*}.${lastVersionPart}
 jq '.version="'${newVersion}'"' ./deploy/metadata.json > tmp.$$.json && mv tmp.$$.json ./deploy/metadata.json
+git add ./deploy/metadata.json
